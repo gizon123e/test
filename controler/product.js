f9efc4ab852f278ee5b7fb1ae5898d33ff476a9a
gizon123e <gizon123e@gmail.com>
2024-04-04 15:25:37 +0700

    fix bug

--- controler/product.js
+++ controler/product.js
@@ -15,10 +15,20 @@ module.exports = {
     },
     upload: async (req, res, next) => {
         try {
-            const { name_product, price, diskon, description, image_product} = req.body
+            const { name_product, price, diskon, description, image_product, stok, categoryId, size, warna, varianRasa, rasaLevel} = req.body
             const total_price = price - (price * diskon /100)
             const user = User.findById(req.user.id).then(async(found)=>{
-                const newProduct = await Product.create({ name_product, price, diskon, description, image_product, total_price, userId:found.id})
+                const newProduct = await Product.create({ 
+                    name_product, 
+                    price, 
+                    diskon, 
+                    description, 
+                    categoryId, 
+                    image_product, 
+                    total_price, 
+                    userId: found.id, 
+                    stok
+                })
                 return res.status(201).json({
                     error: false,
                     message: 'Upload Product Success',
