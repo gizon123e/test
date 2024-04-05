28e456898c3710e8ccde107e522063d6f8c061c5
gizon123e <gizon123e@gmail.com>
2024-04-05 13:06:14 +0700

    edit user-role-clasification

--- midelware/user-role-clasification.js
+++ midelware/user-role-clasification.js
@@ -2,6 +2,7 @@ const User = require('../models/model-auth-user')
 module.exports = async (req, res, next)=>{
     try{
         const user = await User.findById(req.user.id)
+        if(!user) return res.status(404).json({error: true, message: `User dengan id: ${req.user.id} tidak ditemukan`})
         if(user.role.toLowerCase() == "konsumen"){
             return res.status(403).json({error: true, message: "Konsumen tidak bisa manage produk"})
         }
