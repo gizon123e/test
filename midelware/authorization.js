d6ff94dcde3f4bcad35f3e879c13b4c1884055eb
muhammadnurfisyal <161306312+muhammadnurfisyal@users.noreply.github.com>
2024-04-05 13:52:22 +0700

    Update authorization.js

--- midelware/authorization.js
+++ midelware/authorization.js
@@ -7,6 +7,7 @@ module.exports = (req, res, next) => {
         if (!token) return res.status(401).json({ error: false, message: 'Token not Found' })
 
         const verifyToken = jwt.verifyToken(token)
+        console.log(verifyToken)
         if (!verifyToken) return res.status(401).json({ error: false, message: 'authenticate faileds' })
 
         req.user = verifyToken
