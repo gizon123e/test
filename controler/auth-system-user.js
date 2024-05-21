00d20c352a2a93e07a0431f9b688cc3296f99e96
gizon123e <gizon123e@gmail.com>
2024-05-21 10:18:17 +0700

    Menambahkan gambar profil PT BATARA SENA dan PT AINUL YAQIN. modified:   controler/auth-system-user.js modified:   controler/orders.js new file:   public/profile_picts/1716259183632_PT BATARA SENA_.png new file:   public/profile_picts/1716260162365_PT AINUL YAQIN_.png new file:   public/profile_picts/1716260970458_PT AINUL YAQIN_.png new file:   public/profile_picts/1716261420753_PT AINUL YAQIN_.jpg

--- controler/auth-system-user.js
+++ controler/auth-system-user.js
@@ -1,5 +1,6 @@
 const User = require("../models/model-auth-user");
 const UserSystem = require("../models/model-user-system");
+const jwt = require("../utils/jwt")
 const bcrypt = require('bcrypt')
 module.exports = {
     login: async(req, res, next)=>{
@@ -7,12 +8,34 @@ module.exports = {
            const { username, password } = req.body;
            const user = await UserSystem.findOne({username})
            if(!user) return res.status(404).json({message: "User tidak ada"});
-           
+           const payloadToken = {
+                id: user._id,
+                username: user.username,
+                role: user.role
+           }
+           const token = jwt.createToken(payloadToken);
            const validPassword = await bcrypt.compare(password, user.password);
            if(!validPassword) return res.status(401).json({message:"Password Salah"});
-           return res.status(200).json({message: "Login Berhasil", token: "Ini Token"})
+           return res.status(200).json({message: "Login Berhasil", token})
         } catch (error) {
             console.log(error);
+            next(error)
+        }
+    },
+
+    getAllUser: async(req, res, next) =>{
+        try {
+            // if(req.user.role !== "administrator") return res.status(403).json({message: "Hanya Admin yang boleh akses!"});
+
+            const users = await User.aggregate([
+                {
+                    $group: {
+                        role: "konsumen"
+                    }
+                }
+            ])
+            console.log(users);
+        } catch (error) {
             next(error)
         }
     }
