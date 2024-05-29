315263b04f5dfa1bf35adc2fccae145be0223837
gizon123e <gizon123e@gmail.com>
2024-05-29 14:24:03 +0700

    fix respon cart list

--- routes/router-flash-sale.js
+++ routes/router-flash-sale.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const controllerFlashSale = require('../controler/flash-sale')
+const controllerFlashSale = require("../controler/flash-sale");
 
-router.get('/list', controllerFlashSale.getFlashSale);
-router.post('/add', controllerFlashSale.addFlashSale);
+router.get("/list", controllerFlashSale.getFlashSale);
+router.get("/list/admin", controllerFlashSale.getFlashSaleAdmin);
+router.post("/add", controllerFlashSale.addFlashSale);
+router.delete("/delete/:id", controllerFlashSale.deleteFlashSale);
 
-module.exports = router
+module.exports = router;
