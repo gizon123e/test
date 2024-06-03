949059c20e4668f8c61cb8266ac9008b2dc1946a
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-06-03 09:16:20 +0700

    data distributor address kendaraan bisa custom

--- controler/distributtor/kendaraanDistributtor.js
+++ controler/distributtor/kendaraanDistributtor.js
@@ -1,9 +1,9 @@
 const KendaraanDistributor = require('../../models/distributor/model-kendaraanDistributtor')
 const Vendor = require('../../models/vendor/model-vendor')
 const Product = require('../../models/model-product')
-const Konsumen = require('../../models/konsumen/model-konsumen');
-const Gratong = require('../../models/model-gratong')
+const Konsumen = require('../../models/konsumen/model-konsumen')
 const { calculateDistance } = require('../../utils/menghitungJarak')
+const Address = require('../../models/model-address')
 
 module.exports = {
     getKendaraanDistributor: async (req, res, next) => {
@@ -30,82 +30,60 @@ module.exports = {
 
     getKendaraanDistributorById: async (req, res, next) => {
         try {
-            const { id } = req.query
+            const { id, addressId } = req.query
 
             const product = await Product.findOne({ _id: id }).populate('userId')
             const addressVendor = await Vendor.findOne({ userId: product.userId._id }).populate('address')
             const latitudeVebdor = parseFloat(addressVendor.address.pinAlamat.lat)
             const longitudeVendor = parseFloat(addressVendor.address.pinAlamat.long)
 
-            console.log('vendorLat', latitudeVebdor)
-            console.log('vendortLong', longitudeVendor)
-
             const dataKonsumen = await Konsumen.findOne({ userId: req.user.id }).populate("address")
             const latitudeKonsumen = parseFloat(dataKonsumen.address.pinAlamat.lat)
             const longitudeKonsumen = parseFloat(dataKonsumen.address.pinAlamat.long)
 
-            console.log("konsumenLat", latitudeKonsumen)
-            console.log("konsumenLong", longitudeKonsumen)
+            let distance
+            if (addressId) {
+                const addressCustom = await Address.findById(addressId)
+                const latitudeAddressCustom = parseFloat(addressCustom.pinAlamat.lat)
+                const longitudeAdressCustom = parseFloat(addressCustom.pinAlamat.long)
+                distance = calculateDistance(latitudeAddressCustom, longitudeAdressCustom, latitudeVebdor, longitudeVendor, 100);
 
-            const distance = calculateDistance(latitudeKonsumen, longitudeKonsumen, latitudeVebdor, longitudeVendor, 100);
+                if (isNaN(distance)) {
+                    return res.status(400).json({
+                        message: "Jarak antara konsumen dan vendor melebihi 100 km"
+                    });
+                }
+            } else {
+                distance = calculateDistance(latitudeKonsumen, longitudeKonsumen, latitudeVebdor, longitudeVendor, 100);
+
+                if (isNaN(distance)) {
+                    return res.status(400).json({
+                        message: "Jarak antara konsumen dan vendor melebihi 100 km"
+                    });
+                }
+            }
             // const distance = calculateDistance(-6.167350, 106.820926, -6.187499, 106.959382, 100);
 
-            if (isNaN(distance)) {
-                return res.status(400).json({
-                    message: "Jarak antara konsumen dan vendor melebihi 100 km"
-                });
-            }
-
             const jarakTempu = Math.round(distance)
-            console.log(jarakTempu)
 
             let data = []
-            // let isGratong;
             const dataKendaraan = await KendaraanDistributor.find({ id_distributor: req.params.id }).populate('tarifId')
                 .populate({
                     path: "id_distributor",
                     populate: "alamat_id"
                 })
-                .lean()
 
             if (!dataKendaraan) return res.status(404).json({ message: "data Not Found" })
 
             for (let kendaraan of dataKendaraan) {
-                const gratong = await Gratong.findOne({tarif: kendaraan.tarifId._id, startTime: { $lt: new Date() }, endTime: { $gt: new Date() }});
-                
-                let potongan_harga;
-                let total_ongkir;
-
                 if (jarakTempu > 4) {
                     const dataJara = jarakTempu - 4
                     const dataPerKM = kendaraan.tarifId.tarif_per_km * dataJara
                     const hargaOngkir = dataPerKM + kendaraan.tarifId.tarif_dasar
-                    console.log(hargaOngkir)
-                    
-                    if( gratong ){
-                        kendaraan.isGratong = true
-                        switch(gratong.jenis){
-                            case "persentase":
-                                console.log('harga sudah dikurangi diskon :', hargaOngkir * gratong.nilai_gratong / 100)
-                                potongan_harga = hargaOngkir * gratong.nilai_gratong / 100
-                                total_ongkir = hargaOngkir - potongan_harga
-                                break;
-                            case "langsung":
-                                console.log('harga sudah dikurangi diskon :', hargaOngkir - gratong.nilai_gratong)
-                                potongan_harga = hargaOngkir - gratong.nilai_gratong;
-                                total_ongkir = hargaOngkir - potongan_harga;
-                                break;
-                        }
-                    }else{
-                        kendaraan.isGratong = false
-                        total_ongkir = hargaOngkir
-                    }
 
                     data.push({
                         kendaraan,
-                        hargaOngkir,
-                        potongan_harga,
-                        total_ongkir
+                        hargaOngkir
                     })
                 } else {
                     data.push({
@@ -116,9 +94,9 @@ module.exports = {
                 }
             }
 
-            return res.status(200).json({
+            res.status(200).json({
                 message: "get data success",
-                data,
+                data
             })
         } catch (error) {
             console.error("Error creating document:", error);
