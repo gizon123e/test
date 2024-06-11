4c22aa976732a585077b7318a5fe18a9cfa4c851
gizon123e <gizon123e@gmail.com>
2024-06-11 13:15:07 +0700

    change url database

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://mycloudindo123:mycloudindo123@cluster0.lvid7bv.mongodb.net/mycloudindo?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect("mongodb://dev_mycloudindo:dev_mycloudindo@195.7.4.115:27017/mycloudindo")
+// mongoose.connect('mongodb+srv://mycloudindo123:mycloudindo123@cluster0.lvid7bv.mongodb.net/mycloudindo?retryWrites=true&w=majority&appName=Cluster0');
 // mongoose.connect('mongodb://0.0.0.0:27017/mycloudindo')
 
 const db = mongoose.connection;
-
 db.on('error', console.log.bind(console, 'databases connection error'));
 db.on('open', () => console.log('databases connection success'));
