4fe0c5219a7c8542b5b46239c023112f20dda2a2
Alex Tomasoa <alextomasoa@gmail.com>
2024-06-24 22:16:54 +0700

    Set the ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,14 @@ module.exports = {
       {
         name: 'Backend SuperApps',
         exec_mode: 'cluster',
-        instances: 'max',
+        instances: '2',
         script: './app.js',
         max_memory_restart: "500M",
         out_file: "./outbe.log",
         error_file: "./errorbe.log",
         log_date_format: "DD-MM HH:mm:ss Z",
         log_type: "json",
+        autorestart: false,
         watch: true,
         watch_delay: 3000,
         ignore_watch: [
