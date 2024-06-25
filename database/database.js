78884bd366efd0289e0b037ef3215e605dc8ef83
Alex Tomasoa <alextomasoa@gmail.com>
2024-06-25 17:46:01 +0700

    Change database to localhost

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://mycl0ud1nd0:pxXBVisls8FES+r1F5xBdw@localhost:27017/mycl0ud1ndo")
-mongoose.connect('mongodb+srv://mycloudindo123:mycloudindo123@cluster0.lvid7bv.mongodb.net/mycloudindo?retryWrites=true&w=majority&appName=Cluster0');
-//mongoose.connect("mongodb://mycl0ud1nd0:pxXBVisls8FES+r1F5xBdw@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&authSource=mycl0ud1ndo&appName=mongosh+2.2.10")
+//mongoose.connect('mongodb+srv://mycloudindo123:mycloudindo123@cluster0.lvid7bv.mongodb.net/mycloudindo?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(" mongodb://127.0.0.1:27017/mycl0ud1ndo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10")
 
 const db = mongoose.connection;
 db.on('error', console.log.bind(console, 'databases connection error'));
