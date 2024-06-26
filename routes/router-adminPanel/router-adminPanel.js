7e6a1e76420999b50b4979e17a0e2645f0ba4a1a
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-06-26 14:01:12 +0700

    data terbaru

--- routes/router-adminPanel/router-adminPanel.js
+++ routes/router-adminPanel/router-adminPanel.js
@@ -3,6 +3,7 @@ const controlerAdminPanel = require('../../controler/adminPanel/adminPanel')
 
 const router = require('express').Router()
 
+router.get('/biaya-tetap', controlerAdminPanel.getBiayaTetap)
 router.post('/register-panel', controlerAdminPanel.register)
 router.post('/login-panel', controlerAdminPanel.login)
 router.get('/detail-virtual-account/:id', controlerAdminPanel.userDetailId)
