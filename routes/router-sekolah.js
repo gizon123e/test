a72888560620c44cd2170b6722482c8d64246967
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-07-11 15:33:45 +0700

    data teraru skolah get list

--- routes/router-sekolah.js
+++ routes/router-sekolah.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controllerSekolah = require('../controler/sekolah')
 
 router.get('/npsn/:NPSN', controllerSekolah.checkNpsn);
+router.get('/list', controllerSekolah.getAllSkolah)
 
 module.exports = router
