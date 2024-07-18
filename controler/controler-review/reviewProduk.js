d2127f3155c23d8f3ca7118cd72278782af391d5
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-07-18 16:51:37 +0700

    data terbaru

--- controler/controler-review/reviewProduk.js
+++ controler/controler-review/reviewProduk.js
@@ -8,10 +8,11 @@ module.exports = {
     tambahUlasan: async (req, res, next) => {
         try {
             const { komentar_review, nilai_review, id_toko, id_produk, nilai_pengemasan, nilai_kualitas, nilai_keberhasilan } = req.body;
-            const files = req.files
+            const files = req.files;
             const images = files ? files.images : [];
-            const video = files ? files.video : null
+            const video = files ? files.video : [];
 
+            // Memproses image files
             const imagePaths = [];
             if (Array.isArray(images)) {
                 for (const image of images) {
@@ -29,7 +30,8 @@ module.exports = {
                 imagePaths.push(urlImage);
             }
 
-            const videoPaths = []
+            // Memproses video files
+            const videoPaths = [];
             if (Array.isArray(video)) {
                 for (const vid of video) {
                     const namaVideo = `${Date.now()}-${vid.name}`;
@@ -46,37 +48,47 @@ module.exports = {
                 videoPaths.push(urlVideo);
             }
 
-            const datasReviewVendor = await ReviewVendor.find({ id_toko })
-            const indexReviewVendor = datasReviewVendor.length + 1
+            // Menghitung total review vendor
+            const datasReviewVendor = await ReviewVendor.find({ id_toko });
+            const indexReviewVendor = datasReviewVendor.length + 1;
 
-            let poinVendor = 0
-            if (datasReviewVendor) {
+            let poinVendor = 0;
+            if (datasReviewVendor.length > 0) {
                 for (let vendorPoin of datasReviewVendor) {
-                    const data = parseInt(vendorPoin.nilai_pengemasan) + parseInt(vendorPoin.nilai_kualitas) + parseInt(vendorPoin.nilai_keberhasilan)
-                    bagiData = data / 3
-                    poinVendor += bagiData
+                    const data = parseInt(vendorPoin.nilai_pengemasan) + parseInt(vendorPoin.nilai_kualitas) + parseInt(vendorPoin.nilai_keberhasilan);
+                    const bagiData = data / 3;
+                    poinVendor += bagiData;
                 }
             }
-            const hitungVendor = parseInt(nilai_pengemasan) + parseInt(nilai_kualitas) + parseInt(nilai_keberhasilan)
-            const bagiVendor = hitungVendor / 3
-            poinVendor += bagiVendor
+            const hitungVendor = parseInt(nilai_pengemasan) + parseInt(nilai_kualitas) + parseInt(nilai_keberhasilan);
+            const bagiVendor = hitungVendor / 3;
+            poinVendor += bagiVendor;
 
-            const tokoDetail = await TokoVendor.findOne({ _id: id_toko })
-            if (!tokoDetail) return res.status(404).json({ message: "data id_toko Not Fount" })
+            const tokoDetail = await TokoVendor.findOne({ _id: id_toko });
+            if (!tokoDetail) return res.status(404).json({ message: "data id_toko Not Found" });
 
-            const totalReviewVendor = (poinVendor - tokoDetail.nilai_pinalti) / indexReviewVendor
-            console.log(totalReviewVendor)
+            const totalReviewVendor = (poinVendor - tokoDetail.nilai_pinalti) / indexReviewVendor;
 
-            await ReviewVendor.create({ id_toko, nilai_pengemasan: parseInt(nilai_pengemasan), nilai_kualitas: parseInt(nilai_kualitas), nilai_keberhasilan: parseInt(videoPaths), userId: req.user.id })
+            await ReviewVendor.create({ id_toko, nilai_pengemasan: parseInt(nilai_pengemasan), nilai_kualitas: parseInt(nilai_kualitas), nilai_keberhasilan: parseInt(nilai_keberhasilan), userId: req.user.id });
 
             if (totalReviewVendor < 1) {
-                await TokoVendor.findByIdAndUpdate({ _id: id_toko }, { nilai_review: 1 }, { new: true })
+                await TokoVendor.findByIdAndUpdate({ _id: id_toko }, { nilai_review: 1 }, { new: true });
             } else {
-                await TokoVendor.findByIdAndUpdate({ _id: id_toko }, { nilai_review: totalReviewVendor }, { new: true })
+                await TokoVendor.findByIdAndUpdate({ _id: id_toko }, { nilai_review: totalReviewVendor }, { new: true });
             }
 
-            const reviews = await ReviewProduk.find({ id_produk })
-            const indexReviews = reviews.length + 1
+            // Menghitung total review produk
+            const reviews = await ReviewProduk.find({ id_produk });
+            const indexReviews = reviews.length + 1;
+
+            let nilaiPoin = parseInt(nilai_review);
+            if (reviews.length > 0) {
+                for (let review of reviews) {
+                    nilaiPoin += review.nilai_review;
+                }
+            }
+
+            const totalReviewProduk = nilaiPoin / indexReviews;
 
             // Membuat ulasan baru
             const review = new ReviewProduk({
@@ -88,42 +100,20 @@ module.exports = {
                 video: videoPaths
             });
 
-            let nilaiPoin = parseInt(nilai_review)
-            if (reviews) {
-                for (let vendorPoin of reviews) {
-                    nilaiPoin += vendorPoin.nilai_review;
-                }
-            }
             // Menyimpan ulasan ke database
             const savedReview = await review.save();
 
             if (indexReviews > 0) {
-                const totalReview = nilaiPoin / indexReviews
-                if (totalReview < 1) {
-                    await Product.findByIdAndUpdate({ _id: id_produk }, {
-                        $push: { reviews: savedReview._id },
-                        poin_review: 1
-                    }, { new: true, useFindAndModify: false })
-                } else {
-                    await Product.findByIdAndUpdate({ _id: id_produk }, {
-                        $push: { reviews: savedReview._id },
-                        poin_review: totalReview
-                    }, { new: true, useFindAndModify: false })
-                }
-
+                const totalReview = nilaiPoin / indexReviews;
+                await Product.findByIdAndUpdate({ _id: id_produk }, {
+                    $push: { reviews: savedReview._id },
+                    poin_review: totalReview < 1 ? 1 : totalReview
+                }, { new: true, useFindAndModify: false });
             } else {
-                const totalReview = nilaiPoin / 1
-                if (totalReview > 0) {
-                    await Product.findByIdAndUpdate({ _id: id_produk }, {
-                        $push: { reviews: savedReview._id },
-                        poin_review: totalReview
-                    }, { new: true, useFindAndModify: false })
-                } else {
-                    await Product.findByIdAndUpdate({ _id: id_produk }, {
-                        $push: { reviews: savedReview._id },
-                        poin_review: 1
-                    }, { new: true, useFindAndModify: false })
-                }
+                await Product.findByIdAndUpdate({ _id: id_produk }, {
+                    $push: { reviews: savedReview._id },
+                    poin_review: totalReviewProduk < 1 ? 1 : totalReviewProduk
+                }, { new: true, useFindAndModify: false });
             }
 
             res.status(200).json({
@@ -131,15 +121,15 @@ module.exports = {
                 data: savedReview
             });
         } catch (error) {
-            console.log(error)
+            console.log(error);
             if (error && error.name === 'ValidationError') {
                 return res.status(400).json({
                     error: true,
                     message: error.message,
                     fields: error.fields
-                })
+                });
             }
-            next(error)
+            next(error);
         }
     },
 
