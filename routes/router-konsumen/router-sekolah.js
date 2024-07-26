7fa531aa62298e8c6bb86d7a834daf389dbebdc6
gizon123e <gizon123e@gmail.com>
2024-07-26 10:00:42 +0700

    fix detail sekolah by id

--- routes/router-konsumen/router-sekolah.js
+++ routes/router-konsumen/router-sekolah.js
@@ -3,7 +3,7 @@ const controllerSekolah = require('../../controler/konsumen/sekolah')
 const authorization = require("../../midelware/authorization")
 
 router.get('/list/:id', controllerSekolah.getAllSekolah);
-router.get('detail/:id', authorization, controllerSekolah.getDetailSekolah);
+router.get('/detail/:id', authorization, controllerSekolah.getDetailSekolah);
 router.get('/simulasi/:id', controllerSekolah.getByNPSNSekolahSimulasi);
 router.post("/create", authorization, controllerSekolah.createSekolah);
 
