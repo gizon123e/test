e907e04b8ad07681300fdd11acf32039a446f969
dafamahandika <dafamahandika@gmail.com>
2024-07-31 17:33:33 +0700

    ganti conn db

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://mycl0ud1nd0:8zWH%2FhHL23cZxsYZNbMBRbOeM3rrE5kaKSlq0%2BqOx4i3zELjePBTjGr7LaTwENzU@195.7.4.115:27017/mycl0ud1ndo")
-mongoose.connect("mongodb://localhost:27017/mycloudindo")
+mongoose.connect("mongodb://mycl0ud1nd0:8zWH%2FhHL23cZxsYZNbMBRbOeM3rrE5kaKSlq0%2BqOx4i3zELjePBTjGr7LaTwENzU@195.7.4.115:27017/mycl0ud1ndo")
+// mongoose.connect("mongodb://localhost:27017/mycloudindo")
 
 const db = mongoose.connection;
 db.on("error", console.log.bind(console, "databases connection error"));
