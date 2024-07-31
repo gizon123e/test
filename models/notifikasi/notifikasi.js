d4b8d784b914462c772e3cc65419600cba42bd48
dafamahandika <dafamahandika@gmail.com>
2024-07-31 17:30:54 +0700

    notif selesaikan pembayaran (konsumen) dan notif pesanan berhasil (konsumen)

--- models/notifikasi/notifikasi.js
+++ models/notifikasi/notifikasi.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
 const notifikasiSchema = new mongoose.Schema({
-     orderId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Pesanan",
-          required: true,
-     }
-})
+     userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "userId harus di isi"],
+    },
+    invoiceId: {
+     type: mongoose.Types.ObjectId,
+     ref: "Invoice",
+     required: [true, "invoiceId harus di isi"],
+    }, 
+    jenis_invoice: {
+     type: String,
+     enum: ["Subsidi", "Non Subsidi"],
+     message: "{VALUE} is not supported",
+    }, 
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+  },
+);
+
+const Notifikasi = mongoose.model("Notifikasi", notifikasiSchema);
+
+module.exports = Notifikasi;
