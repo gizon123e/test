c54e93be993f599177086dcd7c22f5bb66e09da1
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-02 14:15:49 +0700

    data tester

--- controler/distributtor/pesananDistributor.js
+++ controler/distributtor/pesananDistributor.js
@@ -76,7 +76,7 @@ module.exports = {
                 const transaksi = await Transaksi.find({ id_pesanan: data.orderId._id });
 
                 const invoiceSubsidi = await Invoice.findOne({ id_transaksi: transaksi.find(tr => tr.subsidi == true)._id, });
-                const invoiceTambahan = await Invoice.findOne({ id_transaksi: transaksi.find(tr => tr.subsidi == false)._id, status: "Lunas" });
+                const invoiceTambahan = await Invoice.findOne({ id_transaksi: transaksi.find(tr => tr.subsidi == false)?._id, status: "Lunas" });
 
                 productToDelivers.forEach(prod => {
                     const productId = prod.productId._id.toString();
