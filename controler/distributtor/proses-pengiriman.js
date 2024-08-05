adb4ed723c96ed4e2e6ace9b487009089544ecc3
gizon123e <gizon123e@gmail.com>
2024-08-05 13:54:47 +0700

    mulai penjemputan

--- controler/distributtor/proses-pengiriman.js
+++ controler/distributtor/proses-pengiriman.js
@@ -64,4 +64,16 @@ module.exports = {
             next(error)
         }
     },
+
+    mulaiPenjemputan: async(req, res, next) => {
+        try {
+            const distri = await Distributtor.exists({userId: req.user.id})
+            const prosesPengiriman = await ProsesPengirimanDistributor.findOneAndUpdate({_id: req.params.id, distributorId: distri._id}, { status_distributor: "Sedang dijemput"}, {new: true});
+            if(!prosesPengiriman) return res.status(404).json({message: "Proses pengiriman tidak ditemukan"});
+            return res.status(200).json({message: "Berhasil Memulai Penjemputan"});
+        } catch (error) {
+            console.log(error);
+            next(error)
+        }
+    }
 }
