e5af5ccfc33a6dc9eca0c3f8d290ef4cb8b52e49
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-07 17:06:00 +0700

    data terbaru update update pengiriman

--- routes/router-distributtor/router-pesananDistributor.js
+++ routes/router-distributtor/router-pesananDistributor.js
@@ -5,7 +5,7 @@ const controllerPengiriman = require('../../controler/distributtor/pengiriman')
 
 router.get("/list/:id", authorization, controlerPesananDistributor.getAllPesananDistributor);
 router.get('/detail/:id', authorization, controlerPesananDistributor.getByIdPengirimanDistributor);
-router.put("/request-pickup/:id", authorization, controllerPengiriman.requestPickUp);
+router.put("/request-pickup", authorization, controllerPengiriman.requestPickUp);
 router.put('/terima', controlerPesananDistributor.updateDiTerimaDistributor);
 
 module.exports = router
