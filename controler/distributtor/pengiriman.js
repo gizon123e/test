5c1f7e0dabc2bdd0e0c431f00093e0dcd838d600
gizon123e <gizon123e@gmail.com>
2024-08-08 16:11:54 +0700

    router ajukan penjemputan fix

--- controler/distributtor/pengiriman.js
+++ controler/distributtor/pengiriman.js
@@ -3,17 +3,27 @@ const Pengiriman = require("../../models/model-pengiriman");
 module.exports = {
     requestPickUp: async (req, res, next) => {
         try {
-            const { orderId, id_toko, kode_pengiriman, distributorId } = req.body
-
-            const pengiriman = await Pengiriman.find({ orderId, id_toko, kode_pengiriman });
+            const pengiriman = await Pengiriman.findByIdAndUpdate(req.params.id, { isRequestedToPickUp: true })
             if (!pengiriman) return res.status(404).json({ message: `Pengiriman dengan Id ${req.params.id} tidak ditemukan` })
-
-            await Pengiriman.updateMany({ orderId, id_toko, kode_pengiriman }, { distributorId, status_distributor: "Pesanan terbaru", rejected: false, status_pengiriman: 'diproses' })
-
-            res.status(200).json({ message: "update data success" })
+            return res.status(200).json({ message: "update data success" })
         } catch (error) {
             console.log(error);
-            next(error)
+            next(error);
         }
     }
+    // requestPickUp: async (req, res, next) => {
+    //     try {
+    //         const { orderId, id_toko, kode_pengiriman, distributorId } = req.body
+
+    //         const pengiriman = await Pengiriman.find({ orderId, id_toko, kode_pengiriman });
+    //         if (!pengiriman) return res.status(404).json({ message: `Pengiriman dengan Id ${req.params.id} tidak ditemukan` })
+
+    //         await Pengiriman.updateMany({ orderId, id_toko, kode_pengiriman }, { distributorId, status_distributor: "Pesanan terbaru", rejected: false, status_pengiriman: 'diproses' })
+
+    //         res.status(200).json({ message: "update data success" })
+    //     } catch (error) {
+    //         console.log(error);
+    //         next(error)
+    //     }
+    // }
 }
