6efcd21b92e71fc9c3176d4b7399ffd56cfb0eec
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-09 11:03:38 +0700

    pelacakan konsumen sudah selesai

--- routes/router-konsumen/router-pelacakan.js
+++ routes/router-konsumen/router-pelacakan.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
-
+const authorization = require('../../midelware/authorization')
 const { getTrekingDistributor } = require('../../controler/konsumen/pelacakanDistributorKonsumen')
 
-router.get('/lacak/:id_toko/:id_distributor/:pengirimanId', getTrekingDistributor)
+router.get('/lacak/:id_toko/:id_distributor/:pengirimanId', authorization, getTrekingDistributor)
 
 module.exports = router
