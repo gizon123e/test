465212dd13157209c463adc5072e205927932178
gizon123e <gizon123e@gmail.com>
2024-08-12 11:32:22 +0700

    supplier dan vendor

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config()
-
-mongoose.connect("mongodb://mycl0ud1nd0:8zWH%2FhHL23cZxsYZNbMBRbOeM3rrE5kaKSlq0%2BqOx4i3zELjePBTjGr7LaTwENzU@195.7.4.115:27017/mycl0ud1ndo")
+const mongo_uri = process.env.MONGO_URI
+mongoose.connect(mongo_uri)
 
 const db = mongoose.connection;
 db.on("error", console.log.bind(console, "databases connection error"));
