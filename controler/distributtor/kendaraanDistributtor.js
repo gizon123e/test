1ca2bd5feb035a834f80fb631bac062c7e4f64d3
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-13 16:31:44 +0700

    buat beranda untuk perhitungan total pesanan, dijemput, dan dibatalkan

--- controler/distributtor/kendaraanDistributtor.js
+++ controler/distributtor/kendaraanDistributtor.js
@@ -794,25 +794,32 @@ module.exports = {
 
     getAllpencarianKendaraDiProsesPengiriman: async (req, res, next) => {
         try {
-            console.log("user id ----->", req.user.id)
             const distributor = await Distributtor.findOne({ userId: req.user.id })
             if (!distributor) return res.status(404).json({ message: "distributor not found" })
 
-            const prosesPengiriman = await ProsesPengirimanDistributor.findById(req.params.id).populate('pengirimanId')
-            if (!prosesPengiriman) return res.status(404).json({ message: "proses pesanan not found" })
+            const penentuanWaktu = await ProsesPengirimanDistributor.findById(req.params.id).populate('pengirimanId')
+            if (!penentuanWaktu) return res.status(404).json({ message: "proses pesanan not found" })
 
-            const kendaraaan = await KendaraanDistributor.find({ id_distributor: distributor._id, jenisKendaraan: prosesPengiriman.pengirimanId.id_jenis_kendaraan })
-            if (kendaraaan.length === 0) return res.status(400).json({ message: 'belom ada yang tersedia kendaraanmu' })
+            const prosesPengiriman = await ProsesPengirimanDistributor.find({ distributorId: distributor._id }).populate('pengirimanId')
+            if (prosesPengiriman.length === 0) return res.status(404).json({ message: "proses pesanan saat ini kosong" })
 
-            const datas = kendaraaan.map((item) => {
-                if (item._id.equals(prosesPengiriman.id_kendaraan)) {
-                    return {
-                        ...item.toObject(),
+            const totalWaktu = penentuanWaktu.optimasi_pengiriman * 2
+
+            const datas = []
+            for (let data of prosesPengiriman) {
+                const kendaraaan = await KendaraanDistributor.findOne({ id_distributor: distributor._id, jenisKendaraan: data.pengirimanId.id_jenis_kendaraan })
+
+                const totalWaktuEstimasi = data.optimasi_pengiriman * 2
+
+                if (totalWaktu === totalWaktuEstimasi) {
+                    datas.push({
+                        ...kendaraaan.toObject(),
                         tidak_tersedia: "sedang penjemputan"
-                    };
+                    })
+                } else {
+                    datas.push(kendaraaan)
                 }
-                return item.toObject();
-            });
+            }
 
             res.status(200).json({
                 message: "get data Kendaraan success",
--- controler/konsumen/pelacakanDistributorKonsumen.js
+++ controler/konsumen/pelacakanDistributorKonsumen.js
@@ -9,11 +9,8 @@ module.exports = {
         try {
             const { id_toko, id_distributor, pengirimanId, id_sekolah } = req.params;
 
-            console.log({ id_toko, id_distributor, pengirimanId });
-
             const konsumen = await Sekolah.findOne({ userId: req.user.id, _id: id_sekolah })
             if (!konsumen) return res.status(404).json({ message: 'konsumen tidak ada' })
-            console.log(konsumen._id)
 
             const pengiriman = await ProsesPengirimanDistributor.findOne({ pengirimanId: pengirimanId })
             if (!pengiriman) return res.status(404).json({ message: "pengiriman id not found" })
@@ -21,8 +18,9 @@ module.exports = {
             const toko = await TokoVendor.findById(id_toko)
             if (!toko) return res.status(404).json({ message: "toko vendor id not found" })
 
-            const location = await PelacakanDistributorKonsumen.find({ id_toko: id_toko, id_distributor: id_distributor, id_pesanan: pengiriman._id, id_konsumen: '6695d8d312db45597a24b32e' });
-            // const location = await PelacakanDistributorKonsumen.find({ id_toko: '666689dd30b2f454ab83035f', id_distributor: '666c197fb5c6e1cf1924d722', id_pesanan: '66bac8fa3471fd2bad154613', id_konsumen: '66a21874d6d9472eed93c53a' });
+            const location = await PelacakanDistributorKonsumen.find({ id_toko: id_toko, id_distributor: id_distributor, id_pesanan: pengiriman._id, id_konsumen: id_sekolah })
+                .populate('id_toko').populate('id_distributor').populate('id_pesanan').populate('id_konsumen').populate('id_address')
+
             if (location.length === 0) {
                 return res.status(404).json({ message: 'Lokasi tidak ditemukan' });
             }
