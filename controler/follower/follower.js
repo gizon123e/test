38cb712d383d5e9596fa4e90e2aee90fb33c700f
gizon123e <gizon123e@gmail.com>
2024-08-15 15:18:31 +0700

    follower

--- controler/follower/follower.js
+++ controler/follower/follower.js
@@ -6,14 +6,19 @@ module.exports = {
             const { sellerUserId } = req.body
             if(!sellerUserId) return res.status(400).json({message: "Kirimkan body sellerUserId"});
             const followed = await Follower.exists({sellerUserId, userId: req.user.id});
-            if(followed) return res.status(403).json({message: "Sudah mengikuti toko ini"})
+            if(followed) {
+                Follower.findOneAndDelete({sellerUserId, userId: req.user.id})
+                .then(()=> console.log("Berhasil Menyimpan Follower"))
+                return res.status(200).json({message: "Berhasil Unfollow", follow: false});
+            }
             const user = await User.exists({_id: sellerUserId});
             if(!user) return res.status(400).json({message: "User tidak ditemukan"});
-            const follow = await Follower.create({
+            Follower.create({
                 userId: req.user.id,
                 sellerUserId
-            });
-            return res.status(200).json({message: "Berhasil mengikuti seller", data: follow})
+            })
+            .then(()=> console.log("Berhasil Menyimpan Follower"))
+            res.status(200).json({message: "Berhasil mengikuti seller", follow: true})
         } catch (error) {
             console.log(error);
             next(error)
