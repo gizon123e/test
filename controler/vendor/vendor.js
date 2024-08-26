479f467afd6f67ef10f8d3cb961a149a30e0d796
dafamahandika <dafamahandika@gmail.com>
2024-08-26 13:24:00 +0700

    fix get product vendor dan create vendor supplier adjustment

--- controler/vendor/vendor.js
+++ controler/vendor/vendor.js
@@ -105,7 +105,8 @@ module.exports = {
                     long: long_pin_alamat,
                     lat: lat_pin_alamat
                 },
-                isMain: true
+                isMain: true,
+                isUsed: true,
             };
             
             if (registerAs === "not_individu") {
