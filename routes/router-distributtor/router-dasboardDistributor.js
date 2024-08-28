ecdc827f14e6975907b2341ee357549702ac4503
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-28 13:08:32 +0700

    data terbaru dasboard distributor

--- routes/router-distributtor/router-dasboardDistributor.js
+++ routes/router-distributtor/router-dasboardDistributor.js
@@ -3,6 +3,6 @@ const authorization = require('../../midelware/authorization')
 const { getAllDasboard, getGrafikPerforma } = require('../../controler/distributtor/dasboard')
 
 router.get('/dasboard', authorization, getAllDasboard)
-router.get('/grafik-performa', getGrafikPerforma);
+router.get('/grafik-performa', authorization, getGrafikPerforma);
 
 module.exports = router
