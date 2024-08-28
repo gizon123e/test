9fb67b448c015bd0f28d5539244c77c8bfeded6a
gizon123e <gizon123e@gmail.com>
2024-08-28 09:23:44 +0700

    fix forgot credentials

--- app.js
+++ app.js
@@ -197,9 +197,9 @@ app.use(require("./midelware/error-midelware"));
 
 app.listen(4000, () => {
   // flash_sale_checker.start()
-  // checker_order()
-  // send_notif()
-  // batalPesanan()
+  checker_order()
+  send_notif()
+  batalPesanan()
   console.log("connection express success");
   websocket.listen(8000, () => {
     console.log("Connection websocket success");
