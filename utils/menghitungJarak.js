a3cbac358f9ee59b1e79c1fbafbab0678aa93f2d
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-29 13:52:08 +0700

    perubahaan data untuk menghitung jarak selesai

--- utils/menghitungJarak.js
+++ utils/menghitungJarak.js
@@ -3,9 +3,7 @@ require('dotenv').config()
 
 async function calculateDistance(lat1, lon1, lat2, lon2, maxDistance) {
     try {
-        const response = await axios.get(`https://peta-backend.superdigitalapps.my.id/route/v1/driving/${lon1},${lat1};${lon2},${lat2}?overview=false`);
-
-        // const response = await axios.get(`https://peta-backend.superdigitalapps.my.id/route/v1/driving/106.63464785719901,-6.276579715529846;106.64978671619583,-6.289461823639146?overview=false`);
+        const response = await axios.get(`${process.env.URL_SEND_MAPS}/driving/${lon1},${lat1};${lon2},${lat2}?overview=false`);
 
         const jarak = parseInt(response.data.routes[0].distance) / 1000;
 
