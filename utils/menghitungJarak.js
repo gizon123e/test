82411afd6b23a18da9a4bb845db3992a22405fdd
gizon123e <gizon123e@gmail.com>
2024-08-29 14:33:33 +0700

    filter proses pengiriman

--- utils/menghitungJarak.js
+++ utils/menghitungJarak.js
@@ -4,11 +4,13 @@ require('dotenv').config()
 async function calculateDistance (lat1, lon1, lat2, lon2, maxDistance) {
     try {
         const response = await axios.get(`${process.env.URL_SEND_MAPS}/driving/${lon1},${lat1};${lon2},${lat2}?overview=false`);
+        if(!response.status){
+            return res.status(400).json({message: "Pin Alamat Tidak Benar, pastikan pin alamat tidak terbalik"})
+        }
         const jarak = parseInt(response.data.routes[0].distance) / 1000;
         if (jarak > maxDistance) {
             return NaN;
         }
-        console.log(jarak,)
         return jarak;
     } catch (error) {
         console.error('Error fetching distance:', error);
