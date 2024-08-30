4e905bf74a48cf5f2c0cc34ad83698dad2588967
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-08-30 10:29:59 +0700

    data terbaru

--- routes/router-distributtor/router-dasboardDistributor.js
+++ routes/router-distributtor/router-dasboardDistributor.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const authorization = require('../../midelware/authorization')
-const { getAllDasboard, getGrafikPerforma } = require('../../controler/distributtor/dasboard')
+const { getAllDasboard, getGrafikPerforma, rataPengiriman } = require('../../controler/distributtor/dasboard')
 
 router.get('/dasboard', authorization, getAllDasboard)
 router.get('/grafik-performa', authorization, getGrafikPerforma);
+router.get('/rata-rata-layanan', authorization, rataPengiriman)
 
 module.exports = router
