b71d991b2b292051c6c006eb86441d458ad46a75
gizon123e <gizon123e@gmail.com>
2024-09-02 13:48:02 +0700

    bukti pengiriman di konsumen;

--- utils/menghitungJarak.js
+++ utils/menghitungJarak.js
@@ -5,7 +5,6 @@ async function calculateDistance(lat1, lon1, lat2, lon2, maxDistance) {
     try {
         const response = await axios.get(`${process.env.URL_SEND_MAPS}/driving/${lon1},${lat1};${lon2},${lat2}?overview=false`);
         const jarak = parseInt(response.data.routes[0].distance) / 1000;
-        console.log(jarak)
         if (jarak > maxDistance) {
             return NaN;
         }
