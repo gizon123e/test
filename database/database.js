a275ce9684e859e185fbf095516c2a32334b6dc9
gizon123e <gizon123e@gmail.com>
2024-09-09 17:43:35 +0700

    terbaru

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config()
 const mongo_uri = process.env.MONGO_URI
-mongoose.connect("mongodb+srv://muhammadnurfisyalt:cBGtG6pO7KsovstY@cluster0.r4zv3uh.mongodb.net/mycloudindo?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(mongo_uri)
 
 const db = mongoose.connection;
 db.on("error", console.log.bind(console, "databases connection error"));
