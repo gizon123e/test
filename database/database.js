16c5af7e36df10915d9f80f9c0a724887e5b5d95
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-09-10 14:05:56 +0700

    data terbaru

--- database/database.js
+++ database/database.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config()
 const mongo_uri = process.env.MONGO_URI
-<<<<<<< HEAD
-mongoose.connect("mongodb://mycl0ud1nd0:8zWH%2FhHL23cZxsYZNbMBRbOeM3rrE5kaKSlq0%2BqOx4i3zELjePBTjGr7LaTwENzU@195.7.4.115:27017/mycl0ud1ndo")
-=======
+
+// mongoose.connect("mongodb://mycl0ud1nd0:8zWH%2FhHL23cZxsYZNbMBRbOeM3rrE5kaKSlq0%2BqOx4i3zELjePBTjGr7LaTwENzU@195.7.4.115:27017/mycl0ud1ndo")
+
 mongoose.connect(mongo_uri)
->>>>>>> 42107167195c01a49947097a4a3223bcade9486e
+
 
 const db = mongoose.connection;
 db.on("error", console.log.bind(console, "databases connection error"));
