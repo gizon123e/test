7ecc61e19d1b63c9171c3cfe98d13a251788be8c
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-09-11 17:00:09 +0700

    membuat databes untuk history search dan pagination

--- controler/history-search.js
+++ controler/history-search.js
@@ -13,7 +13,7 @@ module.exports = {
                 query.search = { $regex: search, $options: 'i' }
             }
 
-            const datas = await HistorySearch.find(query).skip(skip).limit(parseInt(limit))
+            const datas = await HistorySearch.find(query).skip(skip).limit(parseInt(limit)).sort({ createdAt: -1 })
 
             res.status(200).json({
                 message: "get data success",
@@ -34,6 +34,9 @@ module.exports = {
 
     createHistorySearch: async (req, res, next) => {
         try {
+            const validate = await HistorySearch.findOne({ search: req.body.search })
+            if (validate) return res.status(400).json({ message: "data ini sudah ada " })
+
             const data = await HistorySearch.create({ search: req.body.search, userId: req.user.id })
 
             res.status(201).json({
