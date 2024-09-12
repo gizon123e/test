05bfa69e3b104f11273d9335c1bdf3c1e8a5a899
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-09-12 14:09:55 +0700

    data terbaru

--- routes/router-campeniProfile/router-mbg.js
+++ routes/router-campeniProfile/router-mbg.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { getAllCampeniProfileMBG, getByIdCampeniProfileMBG, createCampeniProfileMBG, updateCampeniProfileMBG } = require('../../controler/campeniprofile/mbg')
 
-router.get('/list', getAllCampeniProfileMBG)
+router.get('/list-tes', getAllCampeniProfileMBG)
 router.get('/detail/:id', getByIdCampeniProfileMBG)
 router.post('/create', createCampeniProfileMBG)
 router.put('/update/:id', updateCampeniProfileMBG)
