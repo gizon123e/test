47e193515195b1adc73e4e5fa3ca105061e4cca2
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-09-17 09:52:35 +0700

    data terbaru

--- controler/informasi-bantuan/sub-category-judul-informasi.js
+++ controler/informasi-bantuan/sub-category-judul-informasi.js
@@ -1,9 +1,10 @@
+const InformasiBantuanKonsumen = require('../../models/informasi-bantuan/informasi-bantuan-konsumen')
 const SubCategoryInformasiPertanyaan = require('../../models/informasi-bantuan/sub-category-informasibantuan')
 
 module.exports = {
     getSubCategoryInformasiBantuan: async (req, res, next) => {
         try {
-            const data = await SubCategoryInformasiPertanyaan.find({ id_categori_informasi_bantuan: req.paarams.id })
+            const data = await SubCategoryInformasiPertanyaan.find({ id_categori_informasi_bantuan: req.params.id })
 
             res.status(200).json({
                 message: "get data success",
@@ -33,6 +34,52 @@ module.exports = {
                 message: "create data success",
                 data
             })
+        } catch (error) {
+            console.log(error)
+            if (error && error.name === 'ValidationError') {
+                return res.status(400).json({
+                    error: true,
+                    message: error.message,
+                    fields: error.fields
+                })
+            }
+            next(error)
+        }
+    },
+
+    updateSubCategoryInformasiBantuan: async (req, res, next) => {
+        try {
+            const { id_categori_informasi_bantuan, nama } = req.body
+
+            const dataUpdate = await SubCategoryInformasiPertanyaan.findByIdAndUpdate(req.params.id, { id_categori_informasi_bantuan, nama })
+
+            res.status(200).json({
+                message: "update data success",
+                data: dataUpdate
+            })
+        } catch (error) {
+            console.log(error)
+            if (error && error.name === 'ValidationError') {
+                return res.status(400).json({
+                    error: true,
+                    message: error.message,
+                    fields: error.fields
+                })
+            }
+            next(error)
+        }
+    },
+
+    deleteSubCategoryInformasiBantuan: async (req, res, next) => {
+        try {
+            const dataValidate = await SubCategoryInformasiPertanyaan.findOne({ _id: req.paarams.id })
+            if (!dataValidate) {
+                return res.status(404).json({
+                    message: "data Not Found"
+                })
+            }
+
+            await SubCategoryInformasiPertanyaan.deleteOne({})
         } catch (error) {
             console.log(error)
             if (error && error.name === 'ValidationError') {
