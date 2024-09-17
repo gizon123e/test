dc7755825309f836e056d176b08d47593c2bbe29
gizon123e <gizon123e@gmail.com>
2024-09-17 10:33:36 +0700

    balik ke url env db

--- database/database.js
+++ database/database.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv")
 dotenv.config()
 const mongo_uri = process.env.MONGO_URI
 
-mongoose.connect("mongodb://pt_makan_bergizi_gratis:l3b4YpHUhG5mRvvlhywSsqZyMj8B941R@195.7.4.115:27017/pt_mbg")
+// mongoose.connect("mongodb://pt_makan_bergizi_gratis:l3b4YpHUhG5mRvvlhywSsqZyMj8B941R@195.7.4.115:27017/pt_mbg")
 
-// mongoose.connect(mongo_uri)
+mongoose.connect(mongo_uri)
 
 
 const db = mongoose.connection;
