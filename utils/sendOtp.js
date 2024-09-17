d1e923981b942dc934d463d85ff0174cf8d72c4f
gizon123e <gizon123e@gmail.com>
2024-09-17 17:03:17 +0700

    otp prod

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -6,31 +6,6 @@ module.exports = {
 
     //PRODUCTION OTP SEND
 
-    // sendOtp: (email, kode, status) => {
-    //     const subject = 
-    //     status === "login" ? "Login Otp Code" : 
-    //     status === "register" ? "Register Otp Code" : 
-    //     status === "lupa_password" ? "Forgot Password Otp Code" :
-    //     status === "lupa_pin" ? "Forgot Pin Otp Code" :
-    //     "Send Otp Code"
-    //     const transporter = nodemailer.createTransport({
-    //         host: "smtp.gmail.com",
-    //         port: 587,
-    //         secure: false,
-    //         auth: {
-    //             user: process.env.SENDER_EMAIL_PROD,
-    //             pass: process.env.PASSWORD_EMAIL_PROD,
-    //         },
-    //     });
-
-    //     return transporter.sendMail({
-    //         from: process.env.EMAIL_SENDER,
-    //         to: email,
-    //         subject: subject,
-    //         text: `KODE OTP MBG:  ${kode} berlaku selama 5 menit. RAHASIAKAN KODE OTP MBG Anda! Jangan beritahukan kepada SIAPAPUN!`,
-    //     })
-    // },
-
     sendOtp: (email, kode, status) => {
         const subject = 
         status === "login" ? "Login Otp Code" : 
@@ -40,13 +15,11 @@ module.exports = {
         "Send Otp Code"
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
-                type: "OAuth2",
-                user: process.env.EMAIL_SENDER,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
+                user: process.env.SENDER_EMAIL_PROD,
+                pass: process.env.PASSWORD_EMAIL_PROD,
             },
         });
 
@@ -55,15 +28,42 @@ module.exports = {
             to: email,
             subject: subject,
             text: `KODE OTP MBG:  ${kode} berlaku selama 5 menit. RAHASIAKAN KODE OTP MBG Anda! Jangan beritahukan kepada SIAPAPUN!`,
-            auth: {
-                user: process.env.EMAIL_SENDER,
-                refreshToken: process.env.REFRESH_TOKEN,
-                accessToken: process.env.ACCESS_TOKEN,
-                expires: 1484314697598,
-            },
-
         })
     },
+
+    // sendOtp: (email, kode, status) => {
+    //     const subject = 
+    //     status === "login" ? "Login Otp Code" : 
+    //     status === "register" ? "Register Otp Code" : 
+    //     status === "lupa_password" ? "Forgot Password Otp Code" :
+    //     status === "lupa_pin" ? "Forgot Pin Otp Code" :
+    //     "Send Otp Code"
+    //     const transporter = nodemailer.createTransport({
+    //         host: "smtp.gmail.com",
+    //         port: 465,
+    //         secure: true,
+    //         auth: {
+    //             type: "OAuth2",
+    //             user: process.env.EMAIL_SENDER,
+    //             clientId: process.env.CLIENT_ID,
+    //             clientSecret: process.env.CLIENT_SECRET,
+    //         },
+    //     });
+
+    //     return transporter.sendMail({
+    //         from: process.env.EMAIL_SENDER,
+    //         to: email,
+    //         subject: subject,
+    //         text: `KODE OTP MBG:  ${kode} berlaku selama 5 menit. RAHASIAKAN KODE OTP MBG Anda! Jangan beritahukan kepada SIAPAPUN!`,
+    //         auth: {
+    //             user: process.env.EMAIL_SENDER,
+    //             refreshToken: process.env.REFRESH_TOKEN,
+    //             accessToken: process.env.ACCESS_TOKEN,
+    //             expires: 1484314697598,
+    //         },
+
+    //     })
+    // },
 
     sendOtpPhone: async (nomor, content) => {
         try {
