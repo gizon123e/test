b83fafc113837cbb039daf1adf7afca56893ee90
gizon123e <gizon123e@gmail.com>
2024-09-17 09:59:22 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
       {
-        name: 'Backend SuperApps',
+        name: 'Backend MBG Prod',
         exec_mode: 'cluster',
         instances: '2',
         script: './app.js',
         max_memory_restart: "500M",
-        out_file: "./outbe.log",
-        error_file: "./errorbe.log",
+        out_file: "./outbe_mbg_prod.log",
+        error_file: "./errorbe_mbg_prod.log",
         log_date_format: "DD-MM HH:mm:ss Z",
         log_type: "json",
         exp_backoff_restart_delay: 100,
