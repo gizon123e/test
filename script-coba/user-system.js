6385654894f3ee5acf9ede99740be0d74317796b
gizon123e <gizon123e@gmail.com>
2024-09-18 13:45:45 +0700

    delete unnecessary files

--- script-coba/user-system.js
+++
@@ -1,9 +0,0 @@
-require('../database/database')
-const User = require('../models/model-user-system');
-const bcrypt = require('bcrypt')
-
-const password = bcrypt.hash("lexi123", 10).then((password)=>{
-    User.create({username: "lexi", password}).then(()=>{
-        console.log("Berhasil membuat system user")
-    })
-})
